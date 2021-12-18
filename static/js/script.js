d8b17100cb2d69aa53d249f73c860043e54ecaba
Didisimmons <onyema.onyejekwe@gmail.com>
2021-12-18 05:32:44 +0000

    add search bar to index.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({draggable: true});
     $("#copyright").text(new Date().getFullYear());
+    $('#demo-carousel-content').carousel({fullWidth: true});
+  setInterval(function() {
+    $('#demo-carousel-content').carousel('next');
+  }, 2000);
   });
-
-  
