72a2442a3767df028b9cd1770948f753e2fb3351
Didisimmons <onyema.onyejekwe@gmail.com>
2021-12-27 22:25:03 +0000

    add dynamic input field for user to input recipe instructions

--- static/js/script.js
+++ static/js/script.js
@@ -11,11 +11,9 @@ $(document).ready(function(){
   });
 
 
-
-
+var ingredients = document.getElementById('ingredient-list');
 var add_button = document.getElementById('add-more-button');
 var remove_button = document.getElementById('remove-button');
-var ingredients = document.getElementById('ingredient-list');
 
 /* Allow user to input or delete more ingredients on the create recipe page
  * code taken from Abdul Basit at  https://www.youtube.com/watch?v=MLBLsxcB3Dc
@@ -25,7 +23,7 @@ var ingredients = document.getElementById('ingredient-list');
     inputmore.setAttribute('type', 'text');
     inputmore.setAttribute('id', 'ingredients');
     inputmore.setAttribute('name', 'ingredients');
-    inputmore.setAttribute('class', 'input-ingredients');
+    inputmore.setAttribute('class', ' validate input-ingredients');
     inputmore.setAttribute('minlength', '5');
     inputmore.setAttribute('required', 'true');
     inputmore.setAttribute('placeholder', 'insert ingredients');
@@ -37,4 +35,31 @@ remove_button.onclick = function(){
     if(fields.length > 1) {
         ingredients.removeChild(fields[(fields.length) - 1]);
     }
+};
+
+var add_more_directions = document.getElementById('add-more-directions');
+var remove_directions = document.getElementById('delete-directions');
+var recipe_directions = document.getElementById('instruction-list');
+
+/* Allow user to input or delete recipe instructions on the create recipe page
+ * code taken from Abdul Basit at  https://www.youtube.com/watch?v=MLBLsxcB3Dc
+ */
+add_more_directions.onclick = function(){
+  var nextInstruction = document.createElement('textarea');
+  nextInstruction.setAttribute('type', 'text');
+  nextInstruction.setAttribute('id', 'recipe_instructions');
+  nextInstruction.setAttribute('name', 'recipe_instructions');
+  nextInstruction.setAttribute('class', 'materialize-textarea validate instructions');
+  nextInstruction.setAttribute('minlength', '5');
+  nextInstruction.setAttribute('maxlength', '500s');
+  nextInstruction.setAttribute('required', 'true');
+  nextInstruction.setAttribute('placeholder', 'provide directions for the recipe');
+  recipe_directions.appendChild(nextInstruction);
+};
+
+remove_directions.onclick = function(){
+  var instruction_fields = document.getElementsByClassName('instructions');
+  if(instruction_fields.length > 1) {
+    recipe_directions.removeChild(instruction_fields[(instruction_fields.length) - 1]);
+  }
 };
